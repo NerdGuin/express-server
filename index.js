4f12cd0308b5db0a6d777a840976c62d153d383d
Vitor <victor133br@gmail.com>
2023-09-21 22:48:14 -0300

    test

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 const port = 3000;
 const Console = require('./objects/console.js');
-const { connect } = require('./events/dbMananger.js');
+// const { connect } = require('./events/dbMananger.js');
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per window
-    message: {code: 429, message: 'Too many requests, please try again later' }
+    message: { message: 'Too many requests, please try again later' }
 });
 app.use(limiter);
 app.use(require('./events/api.js'));
