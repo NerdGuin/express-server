bff6a7de92dd860d3ce1f069d2716641f55c3723
Vitor <victor133br@gmail.com>
2023-09-21 22:53:30 -0300

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 const port = 3000;
 const Console = require('./objects/console.js');
-// const { connect } = require('./events/dbMananger.js');
+const { connect } = require('./events/dbMananger.js');
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
@@ -14,12 +14,11 @@ app.use(limiter);
 // app.use(require('./events/api.js'));
 
 app.get('/', function (req, res) {
-    res.status(200).send({ message: 'Hi' });
-    // res.send(200, { message: 'Hi' });
+    res.status(200).send({ message: 'Hello!' });
 });
 
 app.listen(port, () => {
-    // connect();
+    connect();
     Console.sendMessage('INFO', `API started on port ${port}`);
 });
 
